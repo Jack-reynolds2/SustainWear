792777d15042ef6cd384740b54200311a2eac2ff
Geesilla <georgiasilla4@gmail.com>
2025-10-30 15:43:50 +0000

    push

--- sustain-wear/src/app/(pages)/(nonAuthed)/layout.tsx
+++ sustain-wear/src/app/(pages)/(nonAuthed)/layout.tsx
@@ -1,14 +1,22 @@
-function Header ( ) {
-  return (       
-    <header className='w-full flex z-99 absolute p-4'>
-        <div className='w-full'>
-            
-        </div>
-        <div className="flex flex-row space-x-3">
-    
-        </div>
+'use client';
+import { ClerkProvider } from "@clerk/nextjs";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
+function Header() {
+  return (
+    <header className="w-full flex z-[99] absolute p-2">
+            <div className="w-full">
+        
+      </div>
+      <div className="flex flex-row space-x-3"></div>
+      
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
     </header>
-  )
+  );
 }
 
 export default function Layout({
@@ -17,10 +25,17 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-        <Header />
-        {children}
-    </>
 
+      
+           
+          <ClerkProvider> 
+
+            <Header />
+            <main>{children}</main>
+          </ClerkProvider>
+          
+      
+     
+    
   );
 }
